89b4354590fe822f9f403f2a9e0a24a17c007cf2
khaidem <khaidemdenish@gmail.com>
2022-03-02 16:33:49 +0530

    realtime change

---
+++ lib/src/firebaseRealTime/pages/homepage.dart
@@ -0,0 +1,76 @@
+import 'dart:developer';
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:foodwifi/src/firebaseRealTime/pages/display.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final fb = FirebaseDatabase.instance;
+  final name = "Name";
+  final newCreate = "newCreate";
+  final newText = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final ref = fb.ref();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase RealTime DataBase'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                controller: newText,
+              ),
+              ElevatedButton(
+                child: const Text('Read Data'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RealTimeDataBase()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.child(newCreate).set(newText.text);
+                },
+                child: const Text('submit'),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    newText.dispose();
+    super.dispose();
+  }
+}
--- lib/src/router/route/app_route.dart
+++ lib/src/router/route/app_route.dart
@@ -3,7 +3,7 @@ import 'package:foodwifi/src/dateTimeRange/pages/calendar.dart';
 import 'package:foodwifi/src/dateTimeRange/pages/calenderangetime.dart';
 
 import 'package:foodwifi/src/dateTimeRange/pages/datetimepicker.dart';
-import 'package:foodwifi/src/firebaseRealTime/pages/DemoRealTime.dart';
+import 'package:foodwifi/src/firebaseRealTime/pages/homepage.dart';
 import 'package:foodwifi/src/flutterflowpage/pages/createStory.page.dart';
 import 'package:foodwifi/src/imagePicker/pages/imagepicker.dart';
 import 'package:foodwifi/src/router/router.dart';
